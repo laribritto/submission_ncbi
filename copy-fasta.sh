63ac0126cbb079d0f5346f66827c0aa3548acae6
Larissa Brito <98621100+laribritto@users.noreply.github.com>
2025-03-19 14:29:07 -0300

    Delete copy-fasta.sh

--- copy-fasta.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p /mnt/c/Users/Cliente/Desktop/mitogenome/Scripts/submission_ncbi/NCBI  # Cria a pasta fasta se não existir
-
-find /mnt/c/Users/Cliente/Desktop/mitogenome/mitofish-novoplasty -type f -name '*.NCBI.txt' -exec cp {} /mnt/c/Users/Cliente/Desktop/mitogenome/Scripts/submission_ncbi/NCBI/ \;
